9e3b024d519bd47f796774c0d4606bbece1feff0
Max Patiiuk <max@patii.uk>
2021-12-07 10:37:09 -0600

    Add an "ensureState" utility function

--- src/index.ts
+++ src/index.ts
@@ -53,3 +53,38 @@ export const generateDispatch = <ACTION extends Action<string>>(
   typeof object[action.type] === 'function'
     ? object[action.type](action as Extract<ACTION, Action<KEY>>)
     : assertExhaustive(action.type as never);
+
+/*
+ * Wrap Action handler in this function to ensure only certain states
+ * Are allowed to emit this action
+ */
+export function ensureState<
+  STATES extends { readonly type: string },
+  ACTION extends { readonly type: string },
+  TYPES extends STATES['type']
+>(
+  emittedFrom: ReadonlyArray<TYPES>,
+  handler: (payload: {
+    readonly state: STATES & { readonly type: TYPES };
+    readonly action: ACTION;
+  }) => STATES
+) {
+  return ({
+    state,
+    action,
+  }: {
+    readonly state: STATES;
+    readonly action: ACTION;
+  }) => {
+    if (emittedFrom.includes(state.type as TYPES))
+      return handler({
+        state: state as STATES & { readonly type: TYPES },
+        action,
+      });
+    else
+      throw new Error(
+        `Action called from state ${state.type}. ` +
+          `Allowed states: ${emittedFrom.join(', ')}`
+      );
+  };
+}
